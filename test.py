deff7ade4fc2dc2f7c6c5bad5d4fdbe6e25ca6b2
Rajesh <rajurajesh6742@gmail.com>
2023-12-05 19:27:29 +0530

    removed test file

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import random
-data=pd.read_excel("Hairstyle dataset.xlsx")
-result=data[(data["Face Shape"]=="Heart") & (data["Gender"]=="Male")]["Hairstyle"]
-random.shuffle(list(result))
-print(result)
